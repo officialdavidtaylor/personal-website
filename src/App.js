57964bbc828ea0ce60a77714065206ca87ead19c
David Taylor <officialdavidtaylor@gmail.com>
2022-04-18 08:08:29 -0700

    minor content tweaks, change a to button

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ function App() {
       <Navbar />
 
       <div class='container'>
-        <div class='jumbotron bg-light p-5 mt-5'>
-          <h1 class='display-4'>Hi 👋, I'm an aspiring developer</h1>
+        <div class='jumbotron bg-light p-3 mt-5'>
+          <h1 class='display-4'>Hi 👋 I'm an aspiring developer</h1>
           <p class='lead'>
-            I thought I wanted to be in hardware for a long time, but I've realized I was wrong.
+            I thought I wanted to be in the hardware industry for a long time, but I've realized I was wrong.
           </p>
           <hr />
           <p>
-            It's been a long road (not really but it sounds dramatic), and I'm finally coming home.<br />
-            I don't know what I want this website to be just yet, so for now it's just filler text.
+            It has felt like a long road to get to the point of recognizing this, but in preparing for the transition it feels like I'm finally coming home.<br />
+            Also I don't know what I want this website to be just yet, so for now it's just filler text.
           </p>
-          <a class='btn btn-primary btn-lg' href='#' role='button'>Learn more</a>
+          <button type='button' class='btn btn-primary btn-lg' onClick={() => alert('Thanks for your interest! I\'ll be adding new content soon!')}>Learn more</button>
         </div>
       </div>
     </>
