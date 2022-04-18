38b5e5d07db5dfd9deda9798b43dee5fb2945136
David Taylor <officialdavidtaylor@gmail.com>
2022-04-18 08:08:08 -0700

    added div as placeholder in document flow

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,18 @@
 
 const Navbar = () => {
   return (
-    <nav class='navbar navbar-expand-lg navbar-dark bg-dark sticky-top'>
-      <span class='navbar-brand mb-0 px-3 h1'>officialdavidtaylor.com</span>
-      <div class='justify-content-end' id='navbarText'>
-        <span class='navbar-text'>
-          in the pursuit of becoming a better developer
-        </span>
-      </div>
-    </nav>
+    <>
+      <nav class='navbar navbar-expand-lg navbar-dark bg-dark fixed-top'>
+        <span class='navbar-brand mb-0 px-3 h1'>officialdavidtaylor.com</span>
+        <div class='justify-content-end' id='navbarText'>
+          <span class='navbar-text'>
+            > in the pursuit of becoming a better developer
+          </span>
+        </div>
+      </nav>
+      {/* Use a div with fixed height to fill the space in the document flow for the Navbar */}
+      <div style={{ height: '50px' }}></div>
+    </>
   )
 }
 
