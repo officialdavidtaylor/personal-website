c4457518339a2c943e931a55503aa78e81c390e3
David Taylor <officialdavidtaylor@gmail.com>
2022-04-28 23:10:08 -0700

    rename to lowercase

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Head from 'next/head';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/navbar';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
